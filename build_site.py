8c8e91331394802ba9c6aa9a69fba589690c7866
armin liiv <armin.liiv96@gmail.com>
2025-12-04 09:56:37 +0200

    update

--- build_site.py
+++ build_site.py
@@ -355,6 +355,16 @@ PAGE_TEMPLATE = """<!DOCTYPE html>
   <meta property="og:description" content="{meta_description}" />
   <meta property="og:type" content="{og_type}" />
   <meta property="og:url" content="{canonical_url}" />
+
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-N97GNWRHHY"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){{dataLayer.push(arguments);}}
+    gtag('js', new Date());
+    gtag('config', 'G-N97GNWRHHY');
+  </script>
+
   <style>
   {base_css}
   </style>
